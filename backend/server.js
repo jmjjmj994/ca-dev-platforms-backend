b2712045146514c7b52a3f572ee1ff04cee77ed2
Jonathan Braaten Mjøs <111571683+jmjjmj994@users.noreply.github.com>
2024-02-08 22:01:59 +0100

    Setting up API routes to our supabase

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import 'dotenv/config';
+import { createClient } from '@supabase/supabase-js';
+const supabase = createClient('https://gnumotcfovtrisrpyswr.supabase.co', process.env.CLIENTKEY);
+const app = express();
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log('Server running on', PORT);
+});
+app.get('/api/cars', async (request, response) => {
+    const { data, error } = await supabase.from('cars').select('*');
+    response.json({ data });
+});
