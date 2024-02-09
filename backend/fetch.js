f6a50cf6f417386d43dcf153583ae7a167c32c33
Jonathan Braaten Mjøs <111571683+jmjjmj994@users.noreply.github.com>
2024-02-10 00:26:30 +0100

    API route for creating data done

---
+++ backend/fetch.js
@@ -0,0 +1,37 @@
+import 'dotenv/config';
+const options = {
+  headers: { 'Content-type': 'application/json' },
+};
+
+const baseUrl = 'http://localhost:3001/api/cars'; //kjører på localhost, må ordnes så det kan gjøres via render url
+const data = async () => {
+  const res = await fetch(baseUrl);
+  const data = await res.json();
+  console.log(data);
+};
+
+const testPost = async (brand, color, price, img) => {
+  try {
+    const res = await fetch(baseUrl, {
+      method: 'POST',
+      headers: options.headers,
+      body: JSON.stringify({
+        brand: brand,
+        color: color,
+        price: price,
+        img: img,
+      }),
+    });
+
+    if (res.ok) {
+      const data = await res.json();
+      console.log(data);
+    } else {
+      console.error('Failed to post data:', res.status, res.statusText);
+    }
+  } catch (error) {
+    console.error('Error during the fetch:', error);
+  }
+};
+
+testPost('Hummer', 'Orange', '20000', '');
--- backend/server.ts
+++ backend/server.ts
@@ -9,6 +9,7 @@ const supabase = createClient(
 );
 //cors
 const app = express();
+app.use(express.json());
 const corsOptions = {
   origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -19,16 +20,42 @@ app.use(cors(corsOptions));
 //Cors
 const PORT = process.env.PORT;
 
+app.get('/', (request, response) => {
+  response.send('cars');
+});
+
 app.get('/api/cars', async (request, response) => {
   const { data, error } = await supabase.from('cars').select('*');
+  if (error)
+    return response.status(404).json({ error: 'Problems fetching data' });
   response.json({ data });
 });
 
-app.post('/api/cars/insert', async (request, response) => {
-  const { error } = await supabase
-    .from('cars')
-    .insert({ id: 1, name: 'Denmark' });
-  if (error) return response.status(404).json({ error: 'Problems' });
+app.post('/api/cars', async (request, response) => {
+  const body = request.body;
+
+  try {
+    if (!body.brand || !body.color || !body.price)
+      return response.status(404).json({ error: 'Props missing' }).end();
+    const { data, error } = await supabase.from('cars').insert([
+      {
+        brand: body.brand,
+        color: body.color,
+        price: body.price,
+        img: body.image,
+      },
+    ]);
+    response.status(201).json({ success: `data inserted successfully` }).end();
+
+    if (error) {
+      console.error('Error inserting data:', error);
+    } else {
+      console.log('Data inserted successfully');
+      response.end();
+    }
+  } catch (error) {
+    console.error('Error during database operation:', error);
+  }
 });
 
 app.listen(PORT, () => {
