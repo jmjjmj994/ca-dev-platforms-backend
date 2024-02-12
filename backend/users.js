4b0a21af562ec054a672f050534d95911f0cf73f
Samail01 <sapug.2001@hotmail.com>
2024-02-12 12:59:32 +0100

    signup route made

--- backend/users.js
+++ backend/users.js
@@ -4,7 +4,24 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = "https://gnumotcfovtrisrpyswr.supabase.co";
 const CLIENTKEY = process.env.CLIENTKEY;
 const supabase = createClient(supabaseUrl, CLIENTKEY);
-const app = express();
-app.post('/signup', async (request, response) => {
+const router = express.Router();
+console.log("hei");
+router.post('/signup', async (request, response) => {
     const { email, password } = request.body;
+    console.log("hei");
+    try {
+        const { user, session, error } = await supabase.auth.signUp({
+            email: email,
+            password: password
+        });
+        if (error) {
+            return response.status(400).json({ error: error.message });
+        }
+        else {
+            response.status(201).json({ message: "User created", user });
+        }
+    }
+    catch (error) {
+        response.status(500).json({ error: "Server error" });
+    }
 });
--- backend/users.ts
+++ backend/users.ts
@@ -7,11 +7,29 @@ const supabaseUrl = "https://gnumotcfovtrisrpyswr.supabase.co";
 const CLIENTKEY = process.env.CLIENTKEY
 const supabase = createClient(supabaseUrl, CLIENTKEY);
 
-const app = express();
+const router = express.Router()
+console.log("hei");
 
-app.post('/signup', async (request, response) => {
+
+router.post('/signup', async (request, response) => {
     const  { email, password } = request.body;
-})
+    console.log("hei");
+    
+    try {
+        const {user, session, error} = await supabase.auth.signUp({
+            email: email,
+            password: password
+        });
+
+        if (error) {
+            return response.status(400).json({error: error.message});
+        } else {
+            response.status(201).json({message: "User created", user});
+        }
+    } catch (error) {
+        response.status(500).json({error: "Server error"}); 
+    }
+});
 
 
 
