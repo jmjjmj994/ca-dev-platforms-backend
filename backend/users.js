09c29107e690285160228975b1737fe34c9307c0
Samail01 <sapug.2001@hotmail.com>
2024-02-12 12:30:46 +0100

    startet with signup code

--- backend/users.js
+++ backend/users.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import 'dotenv/config';
 import { createClient } from '@supabase/supabase-js';
-const supabase = createClient('https://gnumotcfovtrisrpyswr.supabase.co', process.env.CLIENTKEY);
+const supabaseUrl = "https://gnumotcfovtrisrpyswr.supabase.co";
+const CLIENTKEY = process.env.CLIENTKEY;
+const supabase = createClient(supabaseUrl, CLIENTKEY);
 const app = express();
 app.post('/signup', async (request, response) => {
+    const { email, password } = request.body;
 });
--- backend/users.ts
+++ backend/users.ts
@@ -1,17 +1,17 @@
-import express from 'express';
+import express, { response } from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(
-    'https://gnumotcfovtrisrpyswr.supabase.co',
-    process.env.CLIENTKEY
-  );
+const supabaseUrl = "https://gnumotcfovtrisrpyswr.supabase.co";
+const CLIENTKEY = process.env.CLIENTKEY
+const supabase = createClient(supabaseUrl, CLIENTKEY);
 
 const app = express();
 
 app.post('/signup', async (request, response) => {
-    
+    const  { email, password } = request.body;
 })
+
 
 
