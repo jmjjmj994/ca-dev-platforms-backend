5a965e0ebf786cd0d812db08cac08673e5d73ca9
Samail01 <sapug.2001@hotmail.com>
2024-02-12 09:37:40 +0100

    put request fetch added

--- backend/fetch.js
+++ backend/fetch.js
@@ -10,6 +10,7 @@ const data = async () => {
   console.log(data);
 };
 
+//POST request
 const testPost = async (brand, color, price, img) => {
   try {
     const res = await fetch(baseUrl, {
@@ -34,4 +35,30 @@ const testPost = async (brand, color, price, img) => {
   }
 };
 
+//PUT request
+const testPut = async (id, brand, color, price, img) => {
+
+  const putUrl = `${baseUrl}${id}`;
+
+  try {
+    const res = await fetch(putUrl, {
+      method: 'PUT',
+      headers: options.headers,
+      body: JSON.stringify({
+
+      })
+    });
+    
+    if(res.ok) {
+      const data = await res.json();
+      console.log("Updated successfully", data);
+    } else {
+      console.error("Failed to Update data:", res.status, res.statusText);
+    }
+  } catch (error) {
+    console.error("Error during PUT request");
+  }
+};
+
 testPost('Hummer', 'Orange', '20000', '');
+testPut();
